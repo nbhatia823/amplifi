69fc19a0e4476a6516c22e4f38119da95a680043
Alex Thorne <alex@thorne.cc>
2017-09-23 19:47:19 +0100

    Set up Heroku deployment
    * fix dependencies in `package.json`
      * move `devDependencies` to `dependencies` so `webpack` can run on
      Heroku
    * bind to Heroku specified port `process.env.PORT`
    * add `Procfile` to specify run command
    * add `webpack` build step

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ const logreq = (req, res, next) => {
 
 
 /******** RUN SERVER ********/
-const PORT = 3000
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log("Listening on port ", PORT));
