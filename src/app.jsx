fac1327dbac27bf265e49565434204593570f551
Romana Nagyova <r.nagyova22@gmail.com>
2017-09-23 14:27:12 +0100

    resolved merge conflicts

--- src/app.jsx
+++ src/app.jsx
@@ -12,6 +12,58 @@ class App extends Component {
     };
   }
 
+
+  componentDidMount() {
+    this.turnListenerOn();
+  }
+
+  turnListenerOn() {
+    this.setState({
+      listening: true
+    })
+    this.receiver.on('message', x => {
+      console.log(`Received ${x} `);
+      //window.open(x, "_blank");
+    });
+    this.receiver.start();
+  }
+
+  turnListenerOff() {
+    this.reciever.stop();
+    this.setState({
+      listening: false
+    })
+  }
+
+  handleListeningClick() {
+    if(this.state.listening === false) {
+      this.turnListenerOn();
+    }
+    else {
+      this.turnListenerOff();
+    }
+  }
+
+  handleSubmitClick() {
+    console.log('Submit; functionality to be implemented');
+    console.log('Sending message ', this.state.input );
+    this.setState({input: ""});
+  
+    const ssender = new SonicSender();
+       ssender.send(this.state.input);
+  }
+  
+
+  handleSendClick() {
+
+  }
+
+  handleInputChange(event) {
+    this.setState({input: event.target.value});
+    console.log('Input is:', this.state.input);
+  }
+
+
   render() {
     return (
       <div>
@@ -134,9 +186,12 @@ class App extends Component {
         </div>
         <div id="separator"></div>
         <div id="sendSection">
-          <Button id="sendButton" text="Send"/>
-          <input className="send hidden"/>
-          <Button id="submitButton" text="Submit"/>
+           <Button id="sendButton" text="Send" callback={this.handleSendClick}/>
+          <div className="form-group">
+            <label>Message:</label>
+            <input type="text" className="form-control" id="msg" value = {this.state.input} onChange={this.handleInputChange}/>
+          </div>
+          <Button id="submitButton" text="Submit" callback={this.handleSubmitClick}/>
         </div>
       </div>
     );
