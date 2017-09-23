7a4455586b07743652ffac27ad4db86eac8b77b4
Alex Thorne <alex@thorne.cc>
2017-09-23 16:35:34 +0100

    Update project info and build steps

--- server.js
+++ server.js
@@ -24,4 +24,5 @@ const logreq = (req, res, next) => {
 
 
 /******** RUN SERVER ********/
-app.listen(3000, () => console.log("Listening on port 3000."));
+const PORT = 3000
+app.listen(PORT, () => console.log("Listening on port ", PORT));
