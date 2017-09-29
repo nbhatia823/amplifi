f2da3db7b9ef3552be5d4418d3e8d65dae23b89b
Alex Thorne <alex@thorne.cc>
2017-09-29 13:18:45 +0100

    Split particle properties into separate file

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Particles from 'react-particles-js';
 import Sonic from 'sonicnet';
+import particleParams from './particle-params.js'
 
 class App extends Component {
   componentDidMount() {
@@ -95,117 +96,7 @@ class App extends Component {
     return (
         <div>
             <div id="background">
-            <Particles params={{
-              "particles": {
-                "number": {
-                  "value": 135,
-                  "density": {
-                    "enable": false,
-                    "value_area": 1603.40724038582
-                  }
-                },
-                "color": {
-                  "value": "#ffffff"
-                },
-                "shape": {
-                  "type": "triangle",
-                  "stroke": {
-                    "width": 0,
-                    "color": "#000000"
-                  },
-                  "polygon": {
-                    "nb_sides": 3
-                  },
-                  "image": {
-                    "src": "img/github.svg",
-                    "width": 100,
-                    "height": 100
-                  }
-                },
-                "opacity": {
-                  "value": 0.6974821495678316,
-                  "random": true,
-                  "anim": {
-                    "enable": false,
-                    "speed": 1,
-                    "opacity_min": 0.1,
-                    "sync": false
-                  }
-                },
-                "size": {
-                  "value": 3.945745992601726,
-                  "random": true,
-                  "anim": {
-                    "enable": false,
-                    "speed": 40,
-                    "size_min": 0.1,
-                    "sync": false
-                  }
-                },
-                "line_linked": {
-                  "enable": true,
-                  "distance": 205.17879161528975,
-                  "color": "#fff",
-                  "opacity": 0.14204685573366216,
-                  "width": 1
-                },
-                "move": {
-                  "enable": true,
-                  "speed": 1.60340724038582,
-                  "direction": "none",
-                  "random": true,
-                  "straight": false,
-                  "out_mode": "out",
-                  "bounce": false,
-                  "attract": {
-                    "enable": false,
-                    "rotateX": 1122.385068270074,
-                    "rotateY": 1200
-                  }
-                }
-              },
-              "interactivity": {
-                "detect_on": "canvas",
-                "events": {
-                  "onhover": {
-                    "enable": false,
-                    "mode": "repulse"
-                  },
-                  "onclick": {
-                    "enable": false,
-                    "mode": "push"
-                  },
-                  "resize": true
-                },
-                "modes": {
-                  "grab": {
-                    "distance": 400,
-                    "line_linked": {
-                      "opacity": 1
-                    }
-                  },
-                  "bubble": {
-                    "distance": 400,
-                    "size": 40,
-                    "duration": 2,
-                    "opacity": 8,
-                    "speed": 3
-                  },
-                  "repulse": {
-                    "distance": 200,
-                    "duration": 0.4
-                  },
-                  "push": {
-                    "particles_nb": 4
-                  },
-                  "remove": {
-                    "particles_nb": 2
-                  }
-                }
-              },
-              "retina_detect": true
-            }}/>
-
+              <Particles params={particleParams}/>
             </div>
 
             <div id="gradient"></div>
